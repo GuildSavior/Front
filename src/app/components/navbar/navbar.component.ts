afd69054d4d89505bccc5d233bc04f8c39d56efd
clementetiennedev <clement.etienne@ynov.com>
2025-07-09 11:12:36 +0200

    fonctionnel avant crash sur navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -3,11 +3,13 @@ import { LoginService } from '../../services/login/login.service';
 import { Subscription } from 'rxjs';
 import { Router, RouterModule } from '@angular/router';
 import { DiscordAuthService } from '../../services/discordAuth/discord-auth.service';
+import { AuthService } from '../../services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
@@ -15,9 +17,18 @@ export class NavbarComponent {
 
   discordAuth = inject(DiscordAuthService);
   router = inject(Router);
+  auth = inject(AuthService)
   private logoutSubscription: Subscription | null = null;
+  
 
   logout(){
     this.discordAuth.logout();
   }
+  showDashboard = false;
+
+ngOnInit() {
+  this.showDashboard = this.auth.isLoggedIn();
+  console.log('showDashboard:', this.showDashboard);
+}
+
 }
