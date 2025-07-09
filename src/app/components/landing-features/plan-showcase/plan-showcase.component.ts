57f0e01ccab6d0eef9619b7f6d8682b67b4bed6c
clementetiennedev <clement.etienne@ynov.com>
2025-07-09 17:32:42 +0200

    ADD landing page style fini navbar landing fini aussi

---
+++ src/app/components/landing-features/plan-showcase/plan-showcase.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-plan-showcase',
+  standalone: true,
+  imports: [],
+  templateUrl: './plan-showcase.component.html',
+  styleUrl: './plan-showcase.component.scss'
+})
+export class PlanShowcaseComponent {
+
+}
---
+++ src/app/components/tool/whitebar/whitebar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tool-whitebar',
+  standalone: true,
+  imports: [],
+  templateUrl: './whitebar.component.html',
+  styleUrl: './whitebar.component.scss'
+})
+export class WhitebarComponent {
+
+}
