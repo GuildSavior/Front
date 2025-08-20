cab090386c7cd0b46df8a747653e692453c59c22
clementetiennedev <clement.etienne@ynov.com>
2025-08-20 10:21:37 +0200

    Quick update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 // ✅ src/environments/environment.prod.ts (PRODUCTION)
 export const environment = {
   production: true,
-  apiUrl: 'http://82.112.255.241:8080/api',
-  discordAuthUrl: 'http://82.112.255.241:8080/api/auth/discord',
-  appUrl: 'http://82.112.255.241:8080', // ✅ URL de production
-  discordCallbackUrl: 'http://82.112.255.241:8080/auth/discord/callback',
+  apiUrl: 'http://api.guildsavior.online/api',
+  discordAuthUrl: 'http://api.guildsavior.online/api/auth/discord',
+  appUrl: 'http://api.guildsavior.online/', // ✅ URL de production
+  discordCallbackUrl: 'http://api.guildsavior.online/auth/discord/callback',
   enableDebugLogs: false,
   debugMode: false,
   environmentName: 'PRODUCTION'
